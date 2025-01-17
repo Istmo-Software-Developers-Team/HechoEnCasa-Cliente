61c56d1103b424e23410644541c649df3c1dac67
Kr1pt0nmK <137740466+Kr1pt0nmK@users.noreply.github.com>
2025-01-17 00:05:18 -0600

    mejora en la legibilidad y consistencia de los controladores

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InventaryController;
 use App\Http\Controllers\EmergentController;
 use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\CalendaryController;
+use App\Http\Controllers\AlertController;
 
 // Ruta para la página de inicio
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 // Ruta para el calendario
-Route::get('/calendario', [CalendaryController::class, 'index']);
+Route::get('/calendario', [CalendaryController::class, 'index'])->name('calendario.index');
 
 // Ruta para las recetas
-Route::get('/recetas', [RecipeController::class, 'index']);
+Route::get('/recetas', [RecipeController::class, 'index'])->name('recetas.index');
 
-// Ruta para el inventario
-
-Route::get('/ingredientes', [InventaryController::class, 'index'])->name('ingredientes.index');
-Route::patch('/ingredientes/{id_ing}/update-stock', [InventaryController::class, 'updateStock'])->name('ingredientes.updateStock');
-Route::delete('/ingredientes/{id_ing}', [InventaryController::class, 'destroy'])->name('ingredientes.destroy');
-Route::get('/ingredientes/{inventary}', [InventaryController::class, 'getIngrediente'])->name('ingredientes.get');
-Route::post('/ingredientes', [InventaryController::class, 'store'])->name('ingredientes.store');
+// Rutas para el inventario
+Route::prefix('ingredientes')->name('ingredientes.')->group(function () {
+    Route::get('/', [InventaryController::class, 'index'])->name('index');
+    Route::patch('/{id_ing}/update-stock', [InventaryController::class, 'updateStock'])->name('updateStock');
+    Route::delete('/{id_ing}', [InventaryController::class, 'destroy'])->name('destroy');
+    Route::get('/{inventary}', [InventaryController::class, 'getIngrediente'])->name('get');
+    Route::post('/', [InventaryController::class, 'store'])->name('store');
+});
 
 // Ruta para emergentes
-Route::get('/emergentes', [EmergentController::class, 'index']);
+Route::get('/emergentes', [EmergentController::class, 'index'])->name('emergentes.index');
 
-//Ruta para alert
+// Ruta para alertas
 Route::get('/alerts', [AlertController::class, 'index'])->name('alerts.index');
